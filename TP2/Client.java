00664cd15567bf181ccadd159941e429344dcd26
LemonLamo <94323231+LemonLamo@users.noreply.github.com>
2024-02-02 14:46:29 +0100

    added tp2

--- TP2/Client.java
+++ TP2/Client.java
@@ -1,5 +1,74 @@
 package TP2;
 
 public class Client {
-    
+    private int numero;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private String tel;
+    public Client (int numero, String nom, String prenom, String adresse, String tel) {
+        this.numero = numero;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse= adresse;
+        this.tel=tel;
+    }
+     // Constructeur sans argument
+     public Client() {
+        this.numero = 0;
+        this.nom = "";
+        this.prenom = "";
+        this.adresse = "";
+        this.tel = "";
+    }
+    //getters and setters 
+     // Getters
+     public int getNumero() {
+        return numero;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    // Setters
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public String toString() {
+        return "[Le client N° " + numero + " est : " + prenom + " " + nom +
+               " habitant " + adresse + " et son téléphone est " + tel + "]";
+    }
+    public void afficher() {
+        System.out.println(toString());
+    }
 }
+
--- TP2/Tp2exo2.java
+++ TP2/Tp2exo2.java
@@ -1,5 +1,35 @@
 package TP2;
 
 public class Tp2exo2 {
-    
+    public static void main(String[] args) {
+        // Création d'un objet Client avec le constructeur
+        Client client1 = new Client(1, "Koucem", "Lamia", "El biar the best", "0799XXXXXX");
+        
+        // Affichage des informations du client à l'aide de la méthode afficher()
+        System.out.println("\nInformations du client : ");
+        client1.afficher();
+
+        // Création d'un objet Client avec le constructeur sans argument
+        Client client2 = new Client();
+        
+        // Test des méthodes getters
+        System.out.println("\nTest des getters : ");
+        System.out.println("Numéro du client : " + client1.getNumero());
+        System.out.println("Nom du client : " + client1.getNom());
+        System.out.println("Prénom du client : " + client1.getPrenom());
+        System.out.println("Adresse du client : " + client1.getAdresse());
+        System.out.println("Téléphone du client : " + client1.getTel());
+
+        // Test des méthodes setters
+        System.out.println("\nTest des setters : ");
+        client2.setNumero(2);
+        client2.setNom("Hadj Sadok");
+        client2.setPrenom("Riadh");
+        client2.setAdresse("Alger");
+        client2.setTel("0770XXXXXX");
+        // Affichage des nouvelles informations du client
+        System.out.println("\nInformations mises à jour du client : ");
+        client2.afficher();
+    }
 }
+
