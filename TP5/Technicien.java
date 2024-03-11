57bfcb590da138b0e755c1997c30ee5c6bcbe8dc
LemonLamo <94323231+LemonLamo@users.noreply.github.com>
2024-03-12 00:20:05 +0100

    add to tp5

--- TP5/Technicien.java
+++ TP5/Technicien.java
@@ -1,5 +1,32 @@
 package TP5;
 
+import java.util.Vector;
+
 public class Technicien {
-    
+private int matricule; 
+public Specialite specialité; 
+public Vehicule engine;
+private Technicien chefEquipe ;
+private Vector<Technicien> membre;
+
+public int getMatricule() {
+	return this.matricule;
+}
+Technicien(int matricule){
+	this.matricule = matricule;
+}
+
+public void designerChefDequipe(Technicien nouveauChef) {
+	if(nouveauChef !=null) {
+		this.chefEquipe = nouveauChef;
+		for(int i =0 ; i<this.membre.size(); i++) {
+			if(this.membre.elementAt(i).getMatricule() == nouveauChef.getMatricule()) {
+				this.membre.setElementAt(nouveauChef, i);
+			}
+		}
+		return;
+	}else {
+		System.out.println("L\'objet n\'est pas instancié");
+	}
+}
 }
--- TP5/Vehicule.java
+++ TP5/Vehicule.java
@@ -1,5 +1,32 @@
 package TP5;
 
 public class Vehicule {
-    
+private int immatricule;
+private String marque ; 
+private Technicien conducteur;
+
+Vehicule(int immatricule, String marque){
+	this.setImmatricule(immatricule);
+	this.setMarque(marque);
+}
+
+public void ajouterTechnicien(Technicien t){
+	this.conducteur = t;
+}
+
+public String getMarque() {
+	return marque;
+}
+
+public void setMarque(String marque) {
+	this.marque = marque;
+}
+
+public int getImmatricule() {
+	return immatricule;
+}
+
+public void setImmatricule(int immatricule) {
+	this.immatricule = immatricule;
+}
 }
